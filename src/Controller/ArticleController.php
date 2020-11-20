f7524bef89bde15d7d2d0a6eef7f196ddf8715c9
Chancel SIMO <chancelsim@outlook.fr>
2020-11-20 12:23:10 +0100

    controller article home user

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -6,15 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class ArticleController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/article", name="article")
      */
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomePage',
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
         ]);
     }
 }
