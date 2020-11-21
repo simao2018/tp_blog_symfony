8e02a35469e21598c1710f659acf7cca63dd9fda
Chancel SIMO <chancelsim@outlook.fr>
2020-11-21 23:42:52 +0100

    getArticle from repository to paginate

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +16,38 @@ class ArticleController extends AbstractController
     /**
      * @Route("/article", name="article")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository, PaginatorInterface $paginator, Request $req): Response
     {
+        $article = new Article;
+
+
+        $em = $this->getDoctrine()->getManager();
+        // $em->persist($article);
+        // $em->flush();
+
+        $isArticle = true;
+
+        $getArticle = $articleRepository->findAll();
+        $db_query = $this->getDoctrine()->getRepository(Article::class)->findBy([], ['createdAt' => 'desc']);
+        $db_articles = $paginator->paginate(
+            $db_query,
+            $req->query->getInt('page', 1),
+            6
+        );
+
+        if (sizeof($getArticle) < 1) {
+            $isArticle = false;
+        } else {
+            $isArticle = true;
+        }
+
+        dump(sizeof($getArticle));
+
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles' => $getArticle,
+            'isArticle' => $isArticle,
+            'pagination' => $db_articles
         ]);
     }
 }
